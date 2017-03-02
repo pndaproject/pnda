9eba084a5061af648dfd7d9aba367ccc76b44dbe
James Clarke <jamclar2@cisco.com>
2017-03-02 13:12:07 +0000

    Create mirror for logstash plugins
    PNDA-281

--- build/offline/create_mirror_misc.sh
+++ build/offline/create_mirror_misc.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -v
 STATIC_FILE_LIST=$(<pnda-static-file-dependencies.txt)
+PLUGIN_LIST=$(<pnda-logstash-plugin-dependencies.txt)
 
 STATIC_FILE_DIR=$MIRROR_OUTPUT/mirror_misc
 mkdir -p $STATIC_FILE_DIR
@@ -9,3 +10,18 @@ do
     echo $STATIC_FILE
     curl -L -O -J $STATIC_FILE
 done
+
+if [ "x$DISTRO" == "xrhel" ]; then
+    yum install -y java-1.7.0-openjdk
+elif [ "x$DISTRO" == "xubuntu" ]; then
+    apt-get install -y default-jre
+fi
+
+cd /tmp
+curl -LOJ https://download.elastic.co/logstash/logstash/logstash-1.5.4.tar.gz
+tar zxf logstash-1.5.4.tar.gz
+rm logstash-1.5.4.tar.gz
+cd logstash-1.5.4
+bin/plugin install $PLUGIN_LIST
+tar zcf logstash_plugins.tar.gz Gemfile vendor/bundle/jruby/1.9/
+mv logstash_plugins.tar.gz $STATIC_FILE_DIR
--- build/offline/create_mirror_python.sh
+++ build/offline/create_mirror_python.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -v
 export PYTHON_REQ_DIR=$THIS_DIR/requirements
 
-if [ "x$DISTRO" == "xubuntu" ]; then
-	apt-get install libffi-dev python3-pip
+if [ "x$DISTRO" == "xrhel" ]; then
+    yum install -y libffi-devel python34-pip gcc
+elif [ "x$DISTRO" == "xubuntu" ]; then
+    apt-get install -y libffi-dev python3-pip gcc
 fi
 
-wget https://bootstrap.pypa.io/get-pip.py
+curl -LOJ https://bootstrap.pypa.io/get-pip.py
 sudo python get-pip.py
 sudo pip2 install setuptools==34.2.0
 sudo pip2 install github3.py
@@ -13,5 +15,5 @@ sudo easy_install3 pip==9.0.1
 sudo pip3 install setuptools==34.2.0
 sudo rm get-pip.py
 
-python python_download_packages.py
-python python_index_generator.py
+python $THIS_DIR/python_download_packages.py
+python $THIS_DIR/python_index_generator.py
