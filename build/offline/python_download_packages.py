fcf1d4f30b85b5baa07f50c621bc7bf4250d6ed6
James Clarke <jamclar2@cisco.com>
2017-04-06 15:21:46 +0100

    Update python_download_packages.py

--- build/offline/python_download_packages.py
+++ build/offline/python_download_packages.py
@@ -21,13 +21,13 @@ def main():
     subprocess.call(["pip2","download","setuptools","--platform","multilinux1_x86_64","--python-version","27","--implementation","py","--only-binary=:all:","-d",package_path])
     python_deps = (file_py2.read()).rstrip().split('\n')
     for one_dep in python_deps:
-        subprocess.call(["pip2","download",one_dep,"--no-binary",":all:","-d",package_path])
+        subprocess.call(["pip2","download", "--no-deps", one_dep,"--no-binary",":all:","-d",package_path])
 
     # download python 3 libs
     subprocess.call(["pip3","download","setuptools","--only-binary=:all:","-d",package_path])
     python_deps = (file_py3.read()).rstrip().split('\n')
     for one_dep in python_deps:
-        subprocess.call(["pip3","download",one_dep,"-d",package_path])
+        subprocess.call(["pip3","download", "--no-deps", one_dep,"-d",package_path])
 
 if __name__ == '__main__':
     main()
