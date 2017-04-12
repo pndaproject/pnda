9757803c987358be69c198c14dc52b6e0697b2e3
Julien Barbot <jubarbot@cisco.com>
2017-04-12 14:37:34 +0200

    Use fewer commands to find the type of DISTRO

--- build/offline/create_mirror_common.sh
+++ build/offline/create_mirror_common.sh
@@ -1,4 +1,4 @@
-export DISTRO=$(cat /etc/*-release|grep ^ID\=|awk -F\= {'print $2'}|sed s/\"//g)
+export DISTRO=$(awk -F= '/^ID=/ { print $2 }' /etc/*-release|tr -d \")
 
 [[ -z ${MIRROR_BUILD_DIR} ]] && export MIRROR_BUILD_DIR=${PWD}
 [[ -z ${MIRROR_OUTPUT_DIR} ]] && export MIRROR_OUTPUT_DIR=${PWD}/mirror-dist
