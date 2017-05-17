0f7ed65ebf4f76358448b4435dda2c1de79b86de
Jeremie Garnier <je.garnier19@gmail.com>
2017-05-17 13:32:59 +0100

    PNDA-2944 Add RedHat license checks

--- mirror/create_mirror.sh
+++ mirror/create_mirror.sh
@@ -5,6 +5,15 @@ export DISTRO=$(cat /etc/*-release|grep ^ID\=|awk -F\= {'print $2'}|sed s/\"//g)
 [[ -z ${MIRROR_OUTPUT_DIR} ]] && export MIRROR_OUTPUT_DIR=${PWD}/mirror-dist
 
 if [ "x$DISTRO" == "xrhel" ]; then
+	echo "Use of Red Hat software is governed by your agreement with Red Hat."
+	echo "In order to proceed, you must have a valid Red Hat subscription and software image on your system."
+
+	read -p "Do you wish to proceed? [Yes/No]  " yn
+	case $yn in
+	  [Yy]* ) echo "Thanks";;
+	  [Nn]* ) exit;;
+	  * ) echo "Please answer yes or no.";;
+	esac
     $MIRROR_BUILD_DIR/create_mirror_rpm.sh
 elif [ "x$DISTRO" == "xubuntu" ]; then
     $MIRROR_BUILD_DIR/create_mirror_deb.sh
