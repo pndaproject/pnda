728b319f4791deffaae1ade9946023433745da1a
Boopalan S <boopalan.sellappan@maplelabs.com>
2017-08-31 02:58:37 -0400

    fix

--- build/install-build-tools.sh
+++ build/install-build-tools.sh
@@ -103,8 +103,8 @@ if [[ "${DISTRO}" == "rhel" ]]; then
                    python-setuptools \
                    python-devel \
                    python2-pip \                  
-                   libaio \ # Needed for Gobblin
-                   ruby-devel
+                   ruby-devel \
+                   libaio # Needed for Gobblin
 
 elif [[ "${DISTRO}" == "ubuntu" ]]; then
 
@@ -122,8 +122,8 @@ elif [[ "${DISTRO}" == "ubuntu" ]]; then
                    python-setuptools \
                    apt-transport-https \
                    python-pip \
-                   libaio1 \ # Needed for Gobblin
-                   ruby-dev
+                   ruby-dev \
+                   libaio1 # Needed for Gobblin
 fi
 
 if [ ! -f /usr/bin/node ]; then
