2d5d04a2872c3bbb8910b354a4c618716613aa9d
Boopalan S <boopalan.sellappan@maplelabs.com>
2017-09-04 07:48:42 +0000

    changed boopalans to pndaproject repo

--- build/upstream-builds/build-kafkatool.sh
+++ build/upstream-builds/build-kafkatool.sh
@@ -27,7 +27,7 @@ else
 fi
 
 if [[ ${MODE} == "PNDA" ]]; then
-    KT_VERSION=$(wget -qO- https://raw.githubusercontent.com/boopalans/platform-salt/${ARG}/pillar/services.sls | shyaml get-value kafkatool.release_version)
+    KT_VERSION=$(wget -qO- https://raw.githubusercontent.com/pndaproject/platform-salt/${ARG}/pillar/services.sls | shyaml get-value kafkatool.release_version)
 elif [[ ${MODE} == "UPSTREAM" ]]; then
     KT_VERSION=${ARG}
 fi
