46b4cbb3994fe10a1abaeff855c128107df4b984
Donald Hunter <donaldh@cisco.com>
2017-09-05 15:31:28 +0100

    Only prompt about subscription on RHEL

--- mirror/create_mirror.sh
+++ mirror/create_mirror.sh
@@ -4,7 +4,7 @@ export DISTRO=$(cat /etc/*-release|grep ^ID\=|awk -F\= {'print $2'}|sed s/\"//g)
 [[ -z ${MIRROR_BUILD_DIR} ]] && export MIRROR_BUILD_DIR=${PWD}
 [[ -z ${MIRROR_OUTPUT_DIR} ]] && export MIRROR_OUTPUT_DIR=${PWD}/mirror-dist
 
-if [ "x$DISTRO" == "xrhel" -o "x$DISTRO" == "xcentos" ]; then
+if [ "x$DISTRO" == "xrhel" ]; then
 	echo "Use of Red Hat software is governed by your agreement with Red Hat."
 	echo "In order to proceed, you must have a valid Red Hat subscription and software image on your system."
 
@@ -14,6 +14,9 @@ if [ "x$DISTRO" == "xrhel" -o "x$DISTRO" == "xcentos" ]; then
 	  [Nn]* ) exit;;
 	  * ) echo "Please answer yes or no.";;
 	esac
+fi
+
+if [ "x$DISTRO" == "xrhel" -o "x$DISTRO" == "xcentos" ]; then
     $MIRROR_BUILD_DIR/create_mirror_rpm.sh
 elif [ "x$DISTRO" == "xubuntu" ]; then
     $MIRROR_BUILD_DIR/create_mirror_deb.sh
