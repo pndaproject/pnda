ca8bc756c97d3f80e4d8974e6ed0e88e5ba8b99f
James Clarke <jamclar2@cisco.com>
2018-03-26 09:03:21 +0100

    Move version strings into a single versions.sh
    Avoid defining version strings in multiple places by extracting a load of versions into versions.sh and having the other scripts source that.
    
    PNDA-4401

--- mirror/create_mirror_hdp.sh
+++ mirror/create_mirror_hdp.sh
@@ -7,9 +7,10 @@ fi
 
 [[ -z ${MIRROR_BUILD_DIR} ]] && export MIRROR_BUILD_DIR=${PWD}
 [[ -z ${MIRROR_OUTPUT_DIR} ]] && export MIRROR_OUTPUT_DIR=${PWD}/mirror-dist
+source dependencies/versions.sh
 source ${MIRROR_BUILD_DIR}/common/utils.sh
 
-HDP_FILE_LIST=$(<${MIRROR_BUILD_DIR}/dependencies/pnda-hdp-resources.txt)
+HDP_FILE_LIST=$(envsubst < ${MIRROR_BUILD_DIR}/dependencies/pnda-hdp-resources.txt)
 
 HDP_REPO_FILE_DIR=$MIRROR_OUTPUT_DIR/mirror_hdp
 mkdir -p $HDP_REPO_FILE_DIR
@@ -26,15 +27,15 @@ if [ "x$DISTRO" == "xubuntu" ]; then
     apt-key export 'Jenkins (HDP Builds) <jenkin@hortonworks.com>' > hdp.gpg.key
 fi
 
-tar zxf HDP-2.6.4.0-centos7-rpm.tar.gz
-tar zxf HDP-2.6.4.0-ubuntu14-deb.tar.gz
+tar zxf HDP-${HDP_VERSION}-centos7-rpm.tar.gz
+tar zxf HDP-${HDP_VERSION}-ubuntu14-deb.tar.gz
 
-mkdir -p HDP-UTILS-1.1.0.22/repos/ubuntu14/
-tar zxf HDP-UTILS-1.1.0.22-ubuntu14.tar.gz -C 'HDP-UTILS-1.1.0.22/repos/ubuntu14/'
-mkdir -p HDP-UTILS-1.1.0.22/repos/centos7/
-tar zxf HDP-UTILS-1.1.0.22-centos7.tar.gz -C 'HDP-UTILS-1.1.0.22/repos/centos7/'
+mkdir -p HDP-UTILS-${HDP_UTILS_VERSION}/repos/ubuntu14/
+tar zxf HDP-UTILS-${HDP_UTILS_VERSION}-ubuntu14.tar.gz -C "HDP-UTILS-${HDP_UTILS_VERSION}/repos/ubuntu14/"
+mkdir -p HDP-UTILS-${HDP_UTILS_VERSION}/repos/centos7/
+tar zxf HDP-UTILS-${HDP_UTILS_VERSION}-centos7.tar.gz -C "HDP-UTILS-${HDP_UTILS_VERSION}/repos/centos7/"
 
-rm -f HDP-2.6.4.0-centos7-rpm.tar.gz
-rm -f HDP-2.6.4.0-ubuntu14-deb.tar.gz
-rm -f HDP-UTILS-1.1.0.22-centos7.tar.gz
-rm -f HDP-UTILS-1.1.0.22-ubuntu14.tar.gz
+rm -f HDP-${HDP_VERSION}-centos7-rpm.tar.gz
+rm -f HDP-${HDP_VERSION}-ubuntu14-deb.tar.gz
+rm -f HDP-UTILS-${HDP_UTILS_VERSION}-centos7.tar.gz
+rm -f HDP-UTILS-${HDP_UTILS_VERSION}-ubuntu14.tar.gz
--- mirror/create_mirror_misc.sh
+++ mirror/create_mirror_misc.sh
@@ -7,11 +7,11 @@ fi
 
 [[ -z ${MIRROR_BUILD_DIR} ]] && export MIRROR_BUILD_DIR=${PWD}
 [[ -z ${MIRROR_OUTPUT_DIR} ]] && export MIRROR_OUTPUT_DIR=${PWD}/mirror-dist
+source dependencies/versions.sh
 
-export ANACONDA_VERSION="5.1.0"
+STATIC_FILE_LIST=$(envsubst < ${MIRROR_BUILD_DIR}/dependencies/pnda-static-file-dependencies.txt)
+PLUGIN_LIST=$(envsubst < ${MIRROR_BUILD_DIR}/dependencies/pnda-logstash-plugin-dependencies.txt)
 
-STATIC_FILE_LIST=$(<${MIRROR_BUILD_DIR}/dependencies/pnda-static-file-dependencies.txt)
-PLUGIN_LIST=$(<${MIRROR_BUILD_DIR}/dependencies/pnda-logstash-plugin-dependencies.txt)
 source ${MIRROR_BUILD_DIR}/common/utils.sh
 
 STATIC_FILE_DIR=$MIRROR_OUTPUT_DIR/mirror_misc
@@ -19,11 +19,11 @@ mkdir -p $STATIC_FILE_DIR
 cd $STATIC_FILE_DIR
 echo "$STATIC_FILE_LIST" | while read STATIC_FILE
 do
-    eval echo "$STATIC_FILE"
-    eval robust_curl "$STATIC_FILE"
+    echo "$STATIC_FILE"
+    robust_curl "$STATIC_FILE"
 done
-cat SHASUMS256.txt | grep node-v6.10.2-linux-x64.tar.gz > node-v6.10.2-linux-x64.tar.gz.sha1.txt
-sha512sum je-5.0.73.jar > je-5.0.73.jar.sha512.txt
+cat SHASUMS256.txt | grep node-v${NODE_VERSION}-linux-x64.tar.gz > node-v${NODE_VERSION}-linux-x64.tar.gz.sha1.txt
+sha512sum je-${JE_VERSION}.jar > je-${JE_VERSION}.jar.sha512.txt
 sha512sum Anaconda2-${ANACONDA_VERSION}-Linux-x86_64.sh > Anaconda2-${ANACONDA_VERSION}-Linux-x86_64.sh.sha512.txt
 
 if [ "x$DISTRO" == "xrhel" -o "x$DISTRO" == "xcentos" ]; then
@@ -35,7 +35,6 @@ elif [ "x$DISTRO" == "xubuntu" ]; then
 fi
 
 cd /tmp
-LOGSTASH_VERSION=6.2.1
 robust_curl https://artifacts.elastic.co/downloads/logstash/logstash-${LOGSTASH_VERSION}.tar.gz
 tar zxf logstash-${LOGSTASH_VERSION}.tar.gz
 rm logstash-${LOGSTASH_VERSION}.tar.gz
