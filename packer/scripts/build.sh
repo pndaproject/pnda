e3d139540cf6b1f7c4ce978dc785c93489ec80b5
Jeremie Garnier <je.garnier19@gmail.com>
2018-06-21 22:23:56 +0200

    review packer part: rearrange vmware and add AWS

--- packer/scripts/build.sh
+++ packer/scripts/build.sh
@@ -1,20 +1,15 @@
 #!/bin/sh
+function mirror_error {
+    echo "ERROR: $1"
+    exit -1
+}
+
 curl -LOJ $PNDA_REPO/archive/$PNDA_BRANCH.tar.gz
 [[ $? -ne 0 ]] && mirror_error "Problem while getting $PNDA_REPO/archive/$PNDA_BRANCH.tar.gz"
 PB=$PNDA_BRANCH
 CANONICAL_PB=${PB/\\//-}
 tar zxf pnda-$CANONICAL_PB.tar.gz
 ln -s pnda-$CANONICAL_PB pnda
-
-# Build the mirror
-cd pnda/mirror
-sudo mkdir -p /var/www/html/
-export MIRROR_OUTPUT_DIR=/var/www/html
-./create_mirror.sh << EOF
-Yes
-EOF
-
-[[ $? -ne 0 ]] && mirror_error "Problem while creating mirror"
 
 # Install build machine and component package server pre-reqs
 USER=centos
@@ -43,7 +38,9 @@ echo "Before copying to http server home"
 ls -lah /var/www/html/
 ls -lah /home/${USER}/pnda/build/pnda-dist/
 mv /home/${USER}/pnda/build/pnda-dist/* /var/www/html/
-#cp -r /home/${USER}/pnda/mirror/mirror-dist/* /var/www/html/
 
 restorecon -r /var/www/html
 sudo service httpd start
+
+# cleaning up pnda
+sudo rm -rf /home/${USER}/pnda*
---
+++ packer/scripts/mirror.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+function mirror_error {
+    echo "ERROR: $1"
+    exit -1
+}
+
+curl -LOJ $PNDA_REPO/archive/$PNDA_BRANCH.tar.gz
+[[ $? -ne 0 ]] && mirror_error "Problem while getting $PNDA_REPO/archive/$PNDA_BRANCH.tar.gz"
+PB=$PNDA_BRANCH
+CANONICAL_PB=${PB/\\//-}
+tar zxf pnda-$CANONICAL_PB.tar.gz
+ln -s pnda-$CANONICAL_PB pnda
+USER=centos
+
+# Build the mirror
+cd pnda/mirror
+sudo mkdir -p /var/www/html/
+export MIRROR_OUTPUT_DIR=/var/www/html
+./create_mirror.sh << EOF
+Yes
+EOF
+
+[[ $? -ne 0 ]] && mirror_error "Problem while creating mirror"
+
+# cleaning up pnda
+sudo rm -rf /home/${USER}/pnda*
