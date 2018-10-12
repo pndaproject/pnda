c615a83e57592c73066b1b9fa5dacb043b2cad3d
Julien Barbot <jubarbot@cisco.com>
2018-10-12 15:58:46 +0200

    Fix packer script when building a release tag (build)

--- packer/scripts/build.sh
+++ packer/scripts/build.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
+
 function mirror_error {
     echo "ERROR: $1"
     exit -1
 }
 
-rm -rf $PNDA_BRANCH.tar.gz pnda
-curl -LOJ $PNDA_REPO/archive/$PNDA_BRANCH.tar.gz
+TARBALL=pnda.tar.gz
+
+rm -rf $TARBALL pnda
+curl -LsS -o $TARBALL $PNDA_REPO/archive/$PNDA_BRANCH.tar.gz
 [[ $? -ne 0 ]] && mirror_error "Problem while getting $PNDA_REPO/archive/$PNDA_BRANCH.tar.gz"
-PB=$PNDA_BRANCH
-CANONICAL_PB=${PB/\\//-}
-tar zxf pnda-$CANONICAL_PB.tar.gz
-ln -s pnda-$CANONICAL_PB pnda
+
+mkdir pnda
+tar zxf $TARBALL --strip-components=1 -C pnda
 
 # Install build machine and component package server pre-reqs
 
